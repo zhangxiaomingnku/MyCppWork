9fbd6499ab162c9f5b4e6f65e1edea6f9a77dbe4
zhangxiaoming31 <zhangxiaoming31@jd.com>
2021-06-06 22:23:15 +0800

    23,236

---
+++ src/leet_code/236_LCA.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#define LOG_LINE(msg) std::cout << msg << std::endl;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root || root == p || root == q) return root;
+        TreeNode* l = lowestCommonAncestor(root->left, p, q);
+        TreeNode* r = lowestCommonAncestor(root->right, p, q);
+        if(l && r) return root;
+        return l ? l:r;
+    }
+};
